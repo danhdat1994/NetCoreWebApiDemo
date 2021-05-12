820a47407d550d6d3ba11c11b1d188de1b98cba6
dat.nguyen <dat.nguyen@HCM-DATNBD>
2021-05-12 15:32:14 +0700

    Add project files.

---
+++ NetCoreWebApiDemo/CalculatorService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApiDemo
+{
+    public class CalculatorService
+    {
+        public CalculatorService()
+        {
+
+        }
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ XunitDemo/UnitTest1.cs
@@ -0,0 +1,25 @@
+using NetCoreWebApiDemo;
+using System;
+using Xunit;
+
+namespace XunitDemo
+{
+    public class CalculatorServiceTest
+    {
+        private readonly CalculatorService calculatorService;
+        public CalculatorServiceTest()
+        {
+            calculatorService = new CalculatorService();
+        }
+
+        [Fact]
+        public void Add_GiveTwoNumber_ReturnSum()
+        {
+            int a = 1;
+            int b = 2;
+            var result = calculatorService.Add(a, b);
+
+            Assert.Equal(a + b, result);
+        }
+    }
+}
